fb65e179eeac511e02d35e08c9a1a4c93e192ba6
Vesperal-lab <pedro.jangada@escola.pr.gov.br>
2026-08-15 12:10:37 -0300

    Web Eurodit v1.2

--- Eurodit.py
+++ Eurodit.py
@@ -3,6 +3,7 @@ import socket
 from urllib.parse import urlparse
 from urllib.request import Request, urlopen
 from urllib.error import URLError, HTTPError
+from pathlib import Path
 
 
 MESSAGES = {
@@ -61,11 +62,11 @@ print(r"""
      ' `-'   `-' ' '  `-' /  ' `-' ;    '  `-' /  ' `-'  /   | |        '  `-' / ' `-'  /   | |   ' `-' ;  
       '.__.'.__.'   `.__.'    `.__.      `.__.'    '.__.'   (___)        `.__.'   `.__,'   (___)   `.__. 
         
-                                            version 1.1
+                                            version 1.2
     """)
 
 say("start")
-
+BASE_DIR = Path(__file__).resolve().parent
 def starting():
     while True:
         target = input("[+] Usage: Enter the target url\n").strip()
@@ -74,7 +75,6 @@ def starting():
             return target
         say("error")
         print("[X] Invalid URL")
-
 
 def verifyUrl(target):
     request = Request(
@@ -149,6 +149,18 @@ print(fr"""
 """)
 input("PRESS ENTER FOR OPTIONS...")
 
+def directoryChoice():
+    while True:
+        directory = input("[+] Select an option: ").strip().upper().replace(" ", "")
+
+        match directory:
+            case "F" | "D" | "FS" | "DS":
+                return directory
+
+            case _:
+                say("error")
+                print("[X] Invalid option (you can't use the Deep search and Fast search)")
+
 def options():
     while True:
         opt = input(f"""
@@ -158,7 +170,7 @@ def options():
       ┛           
 
     [1] HTTP Headers
-    [2] Directory Enumeration (coming soon...)
+    [2] Directory Enumeration
     [3] WAF Detection (coming soon...)
     [4] Security Checks (coming soon...)
     [5] Exit
@@ -168,21 +180,116 @@ def options():
         match opt:
             case "1":
                 print(f"""
-                [HTTP HEADERS]
+┓┏    ┏┏┓┓  ┓┏     ┓       
+┣┫╋╋┏┓┃┗┓┃  ┣┫┏┓┏┓┏┫┏┓┏┓┏  
+┛┗┗┗┣┛┗┗┛┛  ┛┗┗ ┗┻┗┻┗ ┛ ┛  
 
-                Server: {SERVER}
-                Content-Type: {CONTENT_TYPE}
-                Content-Length: {CONTENT_LENGTH}
-                X-Powered-By: {X_POWERED_BY}
-                Cache-Control: {CACHE_CONTROL}
-                Location: {LOCATION}
-                Set-Cookie: {COOKIES}
+Target: {target}
+Server: {SERVER}
+Content-Type: {CONTENT_TYPE}
+Content-Length: {CONTENT_LENGTH}
+X-Powered-By: {X_POWERED_BY}
+Cache-Control: {CACHE_CONTROL}
+Location: {LOCATION}
+Set-Cookie: {COOKIES}
                 """)
                 input("PRESS ENTER FOR CONTINUE...")
 
             case "2":
-                print("[*] Directory Enumeration coming soon...")
-                input("PRESS ENTER FOR CONTINUE...")
+             print(f"""
+┳┓•              ┏┓              •    
+┃┃┓┏┓┏┓┏╋┏┓┏┓┓┏  ┣ ┏┓┓┏┏┳┓┏┓┏┓┏┓╋┓┏┓┏┓
+┻┛┗┛ ┗ ┗┗┗┛┛ ┗┫  ┗┛┛┗┗┻┛┗┗┗ ┛ ┗┻┗┗┗┛┛┗
+              ┛                       
+OPTIONS: 
+F = "Fast" For a fast search (using a smaller wordlist (recommended))
+D = "Deep" For a deep search (using a bigger wordlist)
+S = "Show All" For show all response url's (including 404)              
+              
+target: {target}
+                """)
+
+             directory = directoryChoice()
+
+
+             match directory:
+                 case "F":
+                     print("[*] Fast scan selected")
+                     haveS = False
+
+                 case "D":
+                     print("[*] Deep scan selected")
+                     haveS = False
+
+                 case "FS":
+                     print("[*] Fast scan selected")
+                     print("[*] Show All enabled")
+                     haveS = True
+
+                 case "DS":
+                     print("[*] Deep scan selected")
+                     print("[*] Show All enabled")
+                     haveS = True
+
+             def simpleVerify(target):
+                 request = Request(target, method="HEAD", headers=headers)
+                 try:
+                     with urlopen(request, timeout=5) as response:
+                         status = response.status
+                         if haveS:
+                             if "adm" in target:
+                                 print(" ")
+                                 say("interesting")
+                             print(" ")
+                             print("[*] FOUND")
+                             print(f"[+] Status code: {status}")
+                             print(f"[+] {target}")
+                             print("__________________________________________________________")
+                             return True
+
+                         elif status != 404:
+                             if "adm" in target:
+                                 say("interesting")
+                             print("[*] FOUND")
+                             print(f"[+] Status code: {status}")
+                             print(f"[+] {target}")
+                             print("__________________________________________________________")
+
+                             return True
+
+
+                         return False
+                 except HTTPError as error:
+                     return False
+                 except (URLError, socket.timeout) as error:
+                     return False
+
+             if "F" in directory:
+                 wordlist = BASE_DIR / "Euromini.txt"
+             elif "D" in directory:
+                 wordlist = BASE_DIR / "Eurolib.txt"
+             def directoryEnum(target, wordlist):
+                tested = 0
+                found = 0
+                try:
+                    with open(wordlist, "r") as file:
+                        for dir in file:
+
+                            path = dir.strip()
+                            url = target.rstrip("/") + "/" + path
+                            print(f"\r[*] Testing: {url}", end="", flush=True)
+                            if simpleVerify(url):
+                                found += 1
+                            tested += 1
+                    print("\n[*] Directory enumeration finished.")
+                    print(f"[*] Paths tested: {tested}")
+                    print(f"[*] Paths tested: {found}")
+                except KeyboardInterrupt:
+                    print("\n")
+                    print("[*] Scan interrupted by user.")
+                    print(f"[*] Paths tested: {tested}")
+                    print(f"[*] Paths tested: {found}")
+             directoryEnum(target, wordlist)
 
             case "3":
                 print("[*] WAF Detection coming soon...")
